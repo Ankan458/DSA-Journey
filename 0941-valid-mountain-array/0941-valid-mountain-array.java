8d0b4b9e2c9f82dbd889832f41808e06fa9f06c3
Ankan <ankanbag2006@gmail.com>
2026-07-10 19:21:54 +0530

    Time: 1 ms (100%), Space: 47.4 MB (52.75%) - LeetHub

---
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        if(arr.length < 3) return false;
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while(left + 1 < right && arr[left] < arr[left + 1]) left++;
+
+        while(right - 1 > 0 && arr[right] < arr[right - 1]) right--;
+
+        return left == right;
+    }
+}
