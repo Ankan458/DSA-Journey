7e80b877251f31a25f1489b7ef4aa773d6ad7d1f
Ankan <ankanbag2006@gmail.com>
2026-07-10 22:12:03 +0530

    Time: 1 ms (99.97%), Space: 42.5 MB (67.68%) - LeetHub

---
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -0,0 +1,39 @@
+class Solution {
+    private int reverse(int num) {
+        int rev = 0;
+
+        while(num != 0) {
+            rev = (rev * 10) + (num % 10);
+            num /= 10;
+        }
+
+        return rev;
+    }
+    public long sumAndMultiply(int n) {
+        if(n == 0) return 0;
+
+        int x = 0;
+
+        while(n != 0) {
+            int digit = n % 10;
+
+            if(digit != 0) {
+                x = (x * 10) + digit;
+            }
+
+            n /= 10;
+        }
+
+        x = reverse(x);
+
+        int sum = 0;
+        int temp = x;
+
+        while (temp != 0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+
+        return (long) x * sum;
+    }
+}
